6f71c0f76cf5367419088ae8bbb8de89b5fcb36d
MartinaVenasco <martinavenasco@gmail.com>
2022-11-04 18:51:08 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         CIAO! MINCHIA, FUNZIONA!
         </a>
       </header>
     </div>
