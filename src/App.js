22d3f795ffd6426aa1d9271a36e9338683df57c5
Waterlilyrr <noemict94@hotmail.it>
2022-11-09 18:36:50 +0100

    feat: Card component created and added to App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import styles from "./app.module.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainSection from "./components/MainSection";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
+      <Card />
       <MainSection />
       <Footer />
     </div>
