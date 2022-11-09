0e3f238726e4e360eebc75659d8438d49090ada2
MartinaVenasco <martinavenasco@gmail.com>
2022-11-09 18:08:43 +0100

    added get artists to main section

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -6,14 +6,15 @@ const MainSection = () => {
   const [artists, setArtists] = useState({});
 
   useEffect(() => {
-    GET("method=library.", "getartists", "&user=joanofarctan&format=json").then((data) =>
-    setArtists((prev) => ({ ...prev, data: data.artists.artist})));
+    GET("method=library.", "getartists", "&user=joanofarctan&format=json").then(
+      (data) => setArtists((prev) => ({ ...prev, data: data.artists.artist }))
+    );
   }, []);
-
 
   return (
     <div className="MainSection">
-      {artists.data && artists.data.map((data, index) => <p key={index}> {data.name} </p>)}
+      {artists.data &&
+        artists.data.map((data, index) => <p key={index}> {data.name} </p>)}
     </div>
   );
 };
