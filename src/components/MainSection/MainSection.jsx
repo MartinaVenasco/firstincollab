3c84142d2540d81c6703d86675247bc4f6a2fbcb
MartinaVenasco <martinavenasco@gmail.com>
2022-11-09 18:27:50 +0100

    fixed mainsection component

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { GET } from "../../utils/api";
 import styles from "./index.module.scss";
 
-const MainSection = ({ modalVisibility, movieEntity, ScrollTop }) => {
+const MainSection = () => {
   const [trackList, setTrackList] = useState([]);
  const [artists, setArtists] = useState({});
  
@@ -19,10 +19,12 @@ const MainSection = ({ modalVisibility, movieEntity, ScrollTop }) => {
 
     <div className={styles.MainSection}>
     <div>
+    <h1>ARTISTS </h1>
       {artists.data &&
         artists.data.map((data, index) => <p key={index}> {data.name} </p>)}
         </div>
             <div>
+            <h1>TRACKS </h1>
       {trackList.data &&
         trackList.data.map((data, index) => <p key={index}>{data.name}</p>)}
     </div>
