12835f64dd01a6e7aa59bce8a44041d5b9719870
Waterlilyrr <noemict94@hotmail.it>
2022-11-10 13:13:26 +0100

    ciao

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,34 +1,39 @@
 import { useState, useEffect } from "react";
 import { GET } from "../../utils/api";
+import { useSelector, useDispatch } from "react-redux";
 import styles from "./index.module.scss";
 
 const MainSection = () => {
-  const [trackList, setTrackList] = useState([]);
- const [artists, setArtists] = useState({});
- 
+  const dispatch = useDispatch();
+  const { trackData } = useSelector((state) => state);
+
+  // const [trackList, setTrackList] = useState([]);
+  // const [artists, setArtists] = useState({});
+
   useEffect(() => {
     GET("method=tag.", "gettoptracks&tag=disco", "&format=json").then((data) =>
-      setTrackList((prev) => ({ ...prev, data: data.tracks.track })));
-
-    GET("method=library.", "getartists", "&user=joanofarctan&format=json").then(
-      (data) => setArtists((prev) => ({ ...prev, data: data.artists.artist }))
+      dispatch({ type: "SET_TRACK_LIST", payload: data.tracks.track })
     );
-  }, []);
+
+    // GET("method=library.", "getartists", "&user=joanofarctan&format=json").then(
+    //   (data) => setArtists((prev) => ({ ...prev, data: data.artists.artist }))
+    // );
+  }, [dispatch]);
 
   return (
-
     <div className={styles.MainSection}>
-    <div>
-    <h1>ARTISTS </h1>
-      {artists.data &&
-        artists.data.map((data, index) => <p key={index}> {data.name} </p>)}
-        </div>
-            <div>
-            <h1>TRACKS </h1>
-      {trackList.data &&
-        trackList.data.map((data, index) => <p key={index}>{data.name}</p>)}
+      {/* <div>
+        <h1>ARTISTS </h1>
+        {artists.data &&
+          artists.data.map((data, index) => <p key={index}> {data.name} </p>)}
+      </div> */}
+      <div>
+        <h1>TRACKS </h1>
+        {trackData?.trackList?.map((data, index) => (
+          <p key={index}>{data.name}</p>
+        ))}
+      </div>
     </div>
-</div>
   );
 };
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,83 +1,64 @@
 import { combineReducers, createStore } from "redux";
 
 const initialState = {
-  cartData: {
-    Uuid: "",
-    cartList: [],
-    purchasedList: [],
+  trackData: {
+    trackList: [],
   },
 
-  modalVisibility: false,
+  // modalVisibility: false,
 
-  loginModalVisibility: false,
+  // loginModalVisibility: false,
 
-  user_data: {
-    userName: "",
-  },
+  // user_data: {
+  //   userName: "",
+  // },
 };
 
-function cartDataReducer(state = {}, action) {
+function trackDataReducer(state = {}, action) {
   switch (action.type) {
-    case "ADD_PRODUCT":
-      state.cartList = [...state.cartList, action.payload];
-      localStorage.setItem("cart", JSON.stringify(state.cartList));
-      return { ...state, cartList: state.cartList };
-    case "REMOVE_PRODUCT":
-      state.cartList = state.cartList.filter((_, id) => id !== action.payload);
-      localStorage.setItem("cart", JSON.stringify(state.cartList));
-      return {
-        ...state,
-        cartList: state.cartList,
-      };
-    case "SET_UUID":
-      return { ...state, Uuid: action.payload };
-    case "BUY_ITEMS":
-      return { ...state, purchasedList: state.cartList };
-    case "CLEAR_PRODUCT":
-      return { ...state, cartList: [] };
-    case "RESTORE_CART":
-      return { ...state, cartList: action.payload };
+    case "SET_TRACK_LIST":
+      return { ...state, trackList: action.payload };
     default:
       return state;
   }
 }
 
-function modalVisibilityChangeReducer(state = {}, action) {
-  switch (action.type) {
-    case "SET_TRUE":
-      return (state = true);
-    case "SET_FALSE":
-      return (state = false);
-    default:
-      return state;
-  }
-}
+// function modalVisibilityChangeReducer(state = {}, action) {
+//   switch (action.type) {
+//     case "SET_TRUE":
+//       return (state = true);
+//     case "SET_FALSE":
+//       return (state = false);
+//     default:
+//       return state;
+//   }
+// }
 
-function loginModalVisibilityReducer(state = {}, action) {
-  switch (action.type) {
-    case "SET_LOGIN_TRUE":
-      return (state = true);
-    case "SET_LOGIN_FALSE":
-      return (state = false);
-    default:
-      return state;
-  }
-}
+// function loginModalVisibilityReducer(state = {}, action) {
+//   switch (action.type) {
+//     case "SET_LOGIN_TRUE":
+//       return (state = true);
+//     case "SET_LOGIN_FALSE":
+//       return (state = false);
+//     default:
+//       return state;
+//   }
+// }
 
-function userDataReducer(state = {}, action) {
-  switch (action.type) {
-    case "SET_USERNAME":
-      return { ...state, userName: action.payload };
-    default:
-      return state;
-  }
-}
+// function userDataReducer(state = {}, action) {
+//   switch (action.type) {
+//     case "SET_USERNAME":
+//       return { ...state, userName: action.payload };
+//     default:
+//       return state;
+//   }
+// }
 
 const rootReducer = combineReducers({
-  cartData: cartDataReducer,
-  modalVisibility: modalVisibilityChangeReducer,
-  loginModalVisibility: loginModalVisibilityReducer,
-  user_data: userDataReducer,
+  trackData: trackDataReducer,
+  // modalVisibility: modalVisibilityChangeReducer,
+  // loginModalVisibility: loginModalVisibilityReducer,
+  // user_data: userDataReducer,
 });
 
 const store = createStore(rootReducer, initialState);
